82c0ccb8d11cdfcb17f39c410ee5b9405a72cf3f
aaron <you@example.com>
2024-02-04 13:45:01 -0800

    fixing yaml_companion

--- lua/plugins/yaml_companion.lua
+++ lua/plugins/yaml_companion.lua
@@ -1,11 +1,41 @@
+-- TODO(aaron): need to configure LSP with this:
+-- https://github.com/someone-stole-my-name/yaml-companion.nvim?tab=readme-ov-file#%EF%B8%8F--configuration
 return {
-  "someone-stole-my-name/yaml-companion.nvim",
-  requires = {
-    { "neovim/nvim-lspconfig" },
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim" },
+  -- add yaml specific modules to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "yaml" })
+      end
+    end,
   },
-  config = function()
-    require("telescope").load_extension("yaml_schema")
-  end,
+  -- yaml schema support
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    version = false, -- last release is way too old
+  },
+
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    requires = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("yaml_schema")
+    end,
+    init = function()
+      local cfg = require("yaml-companion").setup({
+        -- Add any options here, or leave empty to use the default settings
+        -- lspconfig = {
+        --   cmd = {"yaml-language-server"}
+        -- },
+      })
+      require("lspconfig")["yamlls"].setup(cfg)
+    end,
+  },
 }
